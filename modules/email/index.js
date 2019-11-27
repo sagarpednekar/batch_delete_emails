0d359dfdfaccb7e7e1b9c85ed38bd9b54a50c8c2
ronakruparel <ronak.ruparel@accionlabs.com>
2019-11-27 19:44:14 +0530

    Feat(API for get and delete emails)

--- modules/email/index.js
+++ modules/email/index.js
@@ -11,10 +11,11 @@ const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
 fs.readFile("credentials.json", (err, content) => {
-  if (err) return console.log("Error loading client secret file:", err);
-  // Authorize a client with credentials, then call the Gmail API.
-  //   authorize(JSON.parse(content), listLabels);
-  authorize(JSON.parse(content), readEmails);
+   if (err) return console.log("Error loading client secret file:", err);
+   // Authorize a client with credentials, then call the Gmail API.
+   //   authorize(JSON.parse(content), listLabels);
+   authorize(JSON.parse(content), readEmails);
+   //  authorize(JSON.parse(content), deleteEmailById);
 });
 
 /**
@@ -24,19 +25,20 @@ fs.readFile("credentials.json", (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
+   const { client_secret, client_id, redirect_uris } = credentials.installed;
+   const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+   );
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+   // Check if we have previously stored a token.
+   fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getNewToken(oAuth2Client, callback);
+      console.log(JSON.parse(token));
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+   });
 }
 
 /**
@@ -46,28 +48,28 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question("Enter the code from that page here: ", code => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error("Error retrieving access token", err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) return console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
+   const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: SCOPES
+   });
+   console.log("Authorize this app by visiting this url:", authUrl);
+   const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+   });
+   rl.question("Enter the code from that page here: ", code => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+         if (err) return console.error("Error retrieving access token", err);
+         oAuth2Client.setCredentials(token);
+         // Store the token to disk for later program executions
+         fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+            if (err) return console.error(err);
+            console.log("Token stored to", TOKEN_PATH);
+         });
+         callback(oAuth2Client);
       });
-      callback(oAuth2Client);
-    });
-  });
+   });
 }
 
 /**
@@ -76,91 +78,118 @@ function getNewToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listLabels(auth) {
-  const gmail = google.gmail({ version: "v1", auth });
-  gmail.users.labels.list(
-    {
-      userId: "me"
-    },
-    (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      const labels = res.data.labels;
-      if (labels.length) {
-        console.log("Labels:");
-        labels.forEach(label => {
-          console.log(`- ${label.name}`);
-        });
-      } else {
-        console.log("No labels found.");
+   const gmail = google.gmail({ version: "v1", auth });
+   gmail.users.labels.list(
+      {
+         userId: "me"
+      },
+      (err, res) => {
+         if (err) return console.log("The API returned an error: " + err);
+         const labels = res.data.labels;
+         if (labels.length) {
+            console.log("Labels:");
+            labels.forEach(label => {
+               console.log(`- ${label.name}`);
+            });
+         } else {
+            console.log("No labels found.");
+         }
       }
-    }
-  );
+   );
 }
 
 function readEmails(auth) {
-  const gmail = google.gmail({ version: "v1", auth });
-  const labels = [];
-  gmail.users.messages.list(
-    {
-      userId: "me",
-      maxResults: 200
-    },
-    async (err, res) => {
-      //   if (err) console.log(err);
-      //   console.log("--->", res.data.messages);
-      const messages =
-        res && res.data && res.data.messages ? res.data.messages : [];
-      if (messages.length > 0) {
-        const response = new Promise((resolve, reject) => {
-          resolve(
-            messages.map((message, index) => readEmail(message.id, index, auth))
-          );
-        }).then(async array => {
-          console.log(typeof array);
-          Promise.all(array).then(item => {
-            console.log(item);
-          });
-          console.log();
-        });
+   const gmail = google.gmail({ version: "v1", auth });
+   gmail.users.messages.list(
+      {
+         userId: "me",
+         maxResults: 50
+      },
+      (err, res) => {
+         //   if (err) console.log(err);
+         //   console.log("--->", res.data.messages);
+         const messages =
+            res && res.data && res.data.messages ? res.data.messages : [];
+         if (messages.length > 0) {
+            const response = new Promise((resolve, reject) => {
+               resolve(
+                  messages.map((message, index) =>
+                     readEmail(message.id, index, auth)
+                  )
+               );
+            }).then(async array => {
+               console.log("Count", array.length);
+               Promise.all(array)
+                  .then(item => {
+                     console.log("Item", item);
+                  })
+                  .catch(err => {
+                     console.log(err);
+                  });
+            });
+         }
       }
-    }
-  );
+   );
 }
 
 function readEmail(messageId, index, auth) {
-  const gmail = google.gmail({
-    version: "v1",
-    auth
-  });
-  return new Promise((resolve, reject) => {
-    gmail.users.messages.get(
-      {
-        userId: "me",
-        id: messageId
-      },
-      (err, res) => {
-        //   if (err) console.log("Err -->", err);
-        if (res && res.data && res.data.payload && res.data.payload.headers) {
-          // console.log("Reading ...." + index, res.data.snippet);
-          // console.log("Reading ... " + index, res.data.payload.headers);
-          res.data.payload.headers.map((header, index) => {
-            const source = parseEmailBody(header);
-            if (source) {
-              resolve(source);
+   const gmail = google.gmail({
+      version: "v1",
+      auth
+   });
+   return new Promise((resolve, reject) => {
+      gmail.users.messages.get(
+         {
+            userId: "me",
+            id: messageId
+         },
+         (err, res) => {
+            //   if (err) console.log("Err -->", err);
+            if (
+               res &&
+               res.data &&
+               res.data.payload &&
+               res.data.payload.headers
+            ) {
+               // console.log("Reading ...." + index, res.data.snippet);
+               // console.log("Reading ... " + index, res.data.payload.headers);
+               res.data.payload.headers.map((header, index) => {
+                  // console.log("Message ID", messageId);
+                  const source = parseEmailBody(header);
+                  if (source) {
+                     resolve(source);
+                  }
+               });
+            } else {
+               // console.log("Unable to read email");
             }
-          });
-        } else {
-          // console.log("Unable to read email");
-        }
-      }
-    );
-  });
+         }
+      );
+   });
 }
 
 function parseEmailBody(data) {
-  const { name, value } = data;
-  if (name === "From") {
-    // console.log(value.split(" ")[0]);
+   const { name, value } = data;
+   if (name === "From") {
+      // console.log(value.split(" ")[0]);
 
-    return value;
-  }
+      return value;
+   }
+}
+
+function deleteEmailById(auth) {
+   const gmail = google.gmail({
+      version: "v1",
+      auth
+   });
+   return gmail.users.messages.delete(
+      {
+         userId: "me",
+         id: "16ea7c3773839727"
+      },
+      (err, result) => {
+         if (err) console.log(err);
+         console.log("Deleted ", result);
+      }
+   );
 }
--- server.js
+++ server.js
@@ -1,9 +1,134 @@
 const express = require("express");
 const app = express();
 const PORT = 3000 || process.env.PORT;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+const { google } = require("googleapis");
+const auth = require("./modules/email/auth");
+const { authorization } = require("./controllers/auth/index");
+const email = require("./controllers/email/index");
+const { readEmails, deleteEmailById, deleteEmails } = email(google);
 
+// give scope of the passport
+auth(passport);
 
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(
+   cookieSession({
+      name: "session",
+      keys: ["123"]
+   })
+);
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+   res.json({ status: "200" });
+});
+
+app.get(
+   "/auth/google",
+   passport.authenticate("google", {
+      scope: [
+         "https://mail.google.com",
+         "https://www.googleapis.com/auth/userinfo.profile"
+      ],
+      accessType: "offline",
+      approvalPrompt: "force"
+   })
+);
+
+app.get(
+   "/auth/google/callback",
+   passport.authenticate("google", {
+      failureRedirect: "/"
+   }),
+   (req, res) => {
+      console.log(req.user);
+      req.session.token = req.user.token;
+      req.session.refreshToken = req.user.refreshToken;
+
+      const auth = authorization(req.user.token, req.user.refreshToken);
+      google.options({
+         auth
+      });
+
+      res.redirect(
+         "/?token=" +
+            req.user.token +
+            "?refresh_token=" +
+            req.session.refreshToken
+      );
+   }
+);
+
+app.get("/", (req, res) => {
+   if (req.session.token) {
+      res.cookie("token", req.session.token);
+      res.json({
+         status: "session cookie set"
+      });
+   } else {
+      res.cookie("token", "");
+      res.json({
+         status: "session cookie not set"
+      });
+   }
+});
+
+app.get("/list", (req, res) => {
+   const mails = readEmails();
+   mails.then(response => {
+      if (response) {
+         res.json(response);
+      } else {
+         res.json([]);
+      }
+   });
+   //  console.log("Mails ", mails);
+});
+
+app.delete("/delete/email/:id", (req, res) => {
+   const { id } = req.params;
+   console.log(id);
+   if (id) {
+      deleteEmailById(id)
+         .then(response => {
+            res.send(response);
+         })
+         .catch(err => {
+            res.status(404).send(err);
+         });
+   } else {
+      res.status(404).send("Id not found");
+   }
+});
+
+app.post("/batch/delete", (req, res) => {
+   const { ids } = req.body;
+   console.log(ids);
+   if (ids && ids.length) {
+      deleteEmails(ids)
+         .then(result => {
+            res.status(204).send(result);
+         })
+         .catch(err => {
+            res.status(404).send(err);
+         });
+   } else {
+      res.status(404).send("Id's not found");
+   }
+});
+
+app.get("/logout", (req, res) => {
+   req.logout();
+   req.session = null;
+   res.redirect("/");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+   console.log(`Server is running on ${PORT}`);
 });
