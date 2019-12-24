61e26767fdd215a6d6bfbd92ea328b7a3b0a37d1
Sagar Pednekar <sagarpednekar04@gmail.com>
2019-12-25 02:07:20 +0530

    Fix(Redirect URI )

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,17 +1,17 @@
 const { google } = require("googleapis");
-require("dotenv").config()
+require("dotenv").config();
 
 function authorization(access_token, refresh_token) {
-   const oAuth2Client = new google.auth.OAuth2(
-      process.env.client_id,
-      process.env.client_secret,
-      process.env.redirect_uris
-   );
-   oAuth2Client.setCredentials({
-      access_token,
-      refresh_token,
-      token_type: "Bearer"
-   });
-   return oAuth2Client;
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.client_id,
+    process.env.client_secret,
+    "http://pretty-mail.herokuapp.com"
+  );
+  oAuth2Client.setCredentials({
+    access_token,
+    refresh_token,
+    token_type: "Bearer"
+  });
+  return oAuth2Client;
 }
 module.exports = { authorization };
