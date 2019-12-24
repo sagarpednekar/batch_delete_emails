bf7f050ad522fc862a7841234e0e3a0e4a404e01
Sagar Pednekar <sagarpednekar04@gmail.com>
2019-12-24 23:25:28 +0530

    Fix(PORT for heroku)

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT ||4000 ;
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const { google } = require("googleapis");
